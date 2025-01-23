0c5b611cd0e6b9fb56ff89579528b973db4ff2b2
Liam Bagabag <lbscout1@gmail.com>
2025-01-23 12:12:48 -0800

    model work

--- backend/models/build.sh
+++ backend/models/build.sh
@@ -1,6 +1,38 @@
 #!/bin/sh
 
-if [ -d ".venv" ]; then
+model_file=model.py
+server_file=file.py
+
+if [ "$1" = "run" ]; then
+    source .venv/bin/activate
+    if [ "$2" = "$model_file" ]; then
+        python $2 $3
+    else
+        python $2
+    fi
+    deactivate
+    printf "Finished running $1.\n"
+    exit
+fi
+
+if [ "$1" = "serve" ]; then
+    source .venv/bin/activate
+    python -m fastapi run $server_file
+    deactivate
+    printf "Finished running $1.\n"
+    exit
+fi
+
+if [ "$1" = "install" ]; then
+    source .venv/bin/activate
+    pip install $2
+    deactivate
+    printf "Finished installing '$2' package.\n"
+    exit
+fi
+
+
+if [[ ( "$1" = "setup" ) && ( ! -d ".venv" ) ]]; then
     python -m venv .venv
 
     source .venv/bin/activate
