4af7fd540c1cf735f9b9d7056a3745a303766f6a
Fatima Sarr <fatima.sarr01@student.csulb.edu>
2025-02-19 19:13:21 -0800

    fixed environment variable loading

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -6,14 +6,14 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] <- use during production
     #use file with environment variables upon production
-    USER = os.environ['USER']
-    PASSWORD = os.environ['PASSWORD']
-    HOST = os.environ['HOST']
-    PORT = os.environ['PORT']
-    DBNAME = os.environ['DBNAME']
+    USER = os.environ.get('USER')
+    PASSWORD = os.environ.get('PASSWORD')
+    HOST = os.environ.get('HOST')
+    PORT = os.environ.get('PORT')
+    DBNAME = os.environ.get('DBNAME')
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 class ProductionConfig(Config):
