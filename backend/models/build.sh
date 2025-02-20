57f87e5258302a355edb21308a160be18c11bb73
Liam Bagabag <lbscout1@gmail.com>
2025-02-20 10:58:26 -0800

    fixed build.sh

--- backend/models/build.sh
+++ backend/models/build.sh
@@ -1,46 +1,8 @@
 #!/bin/sh
 
-model_file=model.py
-server_file=file-flask.py
+if [ ! -d ".venv" ]; then
+    mkdir -p data
 
-if [ "$1" = "run" ]; then
-    source .venv/bin/activate
-    if [ "$2" = "$model_file" ]; then
-        python $2 $3
-    else
-        python $2
-    fi
-    deactivate
-    printf "Finished running $1.\n"
-    exit
-fi
-
-if [ "$1" = "serve" ]; then
-    source .venv/bin/activate
-    flask --app $server_file run
-    deactivate
-    printf "Finished serving $1 via flask.\n"
-    exit
-fi
-
-if [ "$1" = "debug" ]; then
-    source .venv/bin/activate
-    flask --app $server_file --debug run
-    deactivate
-    printf "Finished serving $1 via flask.\n"
-    exit
-fi
-
-if [ "$1" = "install" ]; then
-    source .venv/bin/activate
-    pip install $2
-    deactivate
-    printf "Finished installing '$2' package.\n"
-    exit
-fi
-
-
-if [[ ( "$1" = "setup" ) && ( ! -d ".venv" ) ]]; then
     python -m venv .venv
 
     source .venv/bin/activate
@@ -48,5 +10,11 @@ if [[ ( "$1" = "setup" ) && ( ! -d ".venv" ) ]]; then
         pip install -r requirements.txt
         deactivate
     fi
-    printf "Finished setting up!\nRun 'source .venv/bin/activate' to use the local python.\n"
+    printf "Finished setting up!\n"
 fi
+
+echo "EXPERIMENTAL: Starting Debug Server!"
+source .venv/bin/activate
+python -m flask --app ./file-flask.py --debug run
+
+deactivate
