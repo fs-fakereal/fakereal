0bef24939117ff359c4351acdbca72e432fd8ae1
letizia <132018538+ohmarcie@users.noreply.github.com>
2025-03-02 14:45:33 -0800

    edited navbar with logo

---
+++ app/static/js/logo-animation.js
@@ -0,0 +1,29 @@
+const anchor = document.getElementById('anchor')
+const rekt = anchor.getBoundingClientRect();
+const anchorX = rekt.left + rekt.width / 2;
+const anchorY = rekt.top + rekt.height / 2;
+const eyes = document.querySelectorAll('.eye')
+
+document.addEventListener('mousemove', (e) => {
+    console.log(e)
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+
+    console.log(angleDeg)
+
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${220 + angleDeg}deg)`;
+        // anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+    })
+})
+
+function angle(cx, cy, ex, ey) {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx); //range (-pi, pi)
+    const deg = rad * 180 / Math.PI;
+    return deg;
+}
