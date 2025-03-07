af055a8a79b50dccbe2bac31aa63ee86cfa9120f
letizia <132018538+ohmarcie@users.noreply.github.com>
2025-03-07 01:33:03 -0800

    edited font and pages

--- app/static/js/carousel.js
+++ app/static/js/carousel.js
@@ -1,41 +1,41 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// next-previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+// // next-previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
 
-// image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// // image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-// show slides
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+// // show slides
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("mySlides");
+//     let dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     }
 
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
 
-    for (i=0; i<slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
+//     for (i=0; i<slides.length; i++) {
+//         slides[i].style.display = 'none';
+//     }
 
-    for (i=0; i<dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+//     for (i=0; i<dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
 
-    slides[slideIndex-1].style.display = "block flex";
-    dots[slideIndex-1].className += " active";
-}
+//     slides[slideIndex-1].style.display = "block flex";
+//     dots[slideIndex-1].className += " active";
+// }
 
 // select the example list
 const wrapper = document.querySelector('.example-list');
---
+++ app/static/js/news-article.js
@@ -0,0 +1,77 @@
+main();
+
+async function main() {
+  const articles = await getData();
+  const articlesUl = document.querySelector('#article');
+
+  // Display only the first 10 articles by default
+  const visibleArticles = articles.slice(0, 10);
+
+  // Loop through the visible articles and add them to the DOM
+  visibleArticles.forEach(article => {
+    articlesUl.innerHTML += 
+    `
+      <div class='article-info'>
+        <a href='${article.url}'><img src="${article.urlToImage ? article.urlToImage : '/static/imgs/FakeReal.png'}" alt="article-img"></a>
+
+        <div class='content'>
+          <a href='${article.url}'><h3>${article.title}</h3></a>
+
+          <div class='author-release'>
+            <p>${article.author ? article.author.split(',').slice(0, 2).join(',') : "Anonymous"}</p>
+            <p>${article.publishedAt}</p>
+          </div>
+
+          <h4>${article.description}</h4>
+        </div>
+      </div>
+    `;
+  });
+
+  // If there are more than 10 articles, add a "Show More" button
+  if (articles.length > 10) {
+    articlesUl.innerHTML += `
+      <button id="show-more">Show More</button>
+    `;
+
+    // Show the remaining articles when the button is clicked
+    const showMoreButton = document.querySelector('#show-more');
+    showMoreButton.addEventListener('click', () => {
+      
+      // Hide the "Show More" button after showing all articles
+      showMoreButton.style.display = 'none';
+      
+      // Display the rest of the articles
+      const hiddenArticles = articles.slice(10);
+      hiddenArticles.forEach(article => {
+        articlesUl.innerHTML += 
+        `
+          <div class='article-info'>
+            <a href='${article.url}'><img class='overlay' src="${article.urlToImage ? article.urlToImage : '/static/imgs/FakeReal.png'}" alt="article-img"></a>
+
+            <div class='content'>
+              <a href='${article.url}'><h3>${article.title}</h3></a>
+
+              <div class='author-release'>
+                <p>${article.author ? article.author.split(',').slice(0, 2).join(', ') : "Anonymous"}</p>
+                <p>${article.publishedAt}</p>
+              </div>
+
+              <h4>${article.description}</h4>
+            </div>
+          </div>
+        `;
+      });
+
+      // Hide the "Show More" button after showing all articles
+      showMoreButton.style.display = 'none';
+    });
+  }
+}
+
+async function getData() {
+  const respo = await fetch("../static/json/news.json");
+  const data = await respo.json();
+
+  return data;
+}
