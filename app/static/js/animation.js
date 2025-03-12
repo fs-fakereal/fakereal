761e163a2bc47ef87fb264e231ed8ccc41489493
letizia <132018538+ohmarcie@users.noreply.github.com>
2025-03-11 21:24:47 -0700

    edited pages

--- app/static/js/animation.js
+++ app/static/js/animation.js
@@ -1,6 +1,6 @@
 // scroll
 let animation = {
-    revealDistance: 150,
+    revealDistance: 25,
     initialOpacity: 0,
     transitionDelay: 0,
     transitionDuration: '2s',
@@ -25,3 +25,60 @@ function reveal() {
 
 window.addEventListener('scroll', reveal);
 window.addEventListener('load', reveal);
+
+// for scaling
+const scaleContainers = document.querySelectorAll('.scale');
+
+function scalable() {
+    for (i=0; i<scaleContainers.length; i++) {
+        let windowHeight = window.innerHeight;
+        let topOfScaleContainer = scaleContainers[i].getBoundingClientRect().top;
+
+        if (topOfScaleContainer < windowHeight - animation.revealDistance) {
+            scaleContainers[i].classList.add('active');
+        } else {
+            scaleContainers[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', scalable);
+window.addEventListener('load', scalable);
+
+// nav scale
+const navScaleContainers = document.querySelectorAll('.nav-scale');
+
+function navScale() {
+    for (i=0; i<navScaleContainers.length; i++) {
+        let windowHeight = window.innerHeight;
+        let topOfScaleContainer = navScaleContainers[i].getBoundingClientRect().top;
+
+        if (topOfScaleContainer < windowHeight - animation.revealDistance) {
+            navScaleContainers[i].classList.add('active');
+        } else {
+            navScaleContainers[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', navScale);
+window.addEventListener('load', navScale);
+
+// examples scale
+const exampleScaleContainers = document.querySelectorAll('.ex-scale');
+
+function exScale() {
+    for (i=0; i<exampleScaleContainers.length; i++) {
+        let windowHeight = window.innerHeight;
+        let topOfScaleContainer = exampleScaleContainers[i].getBoundingClientRect().top;
+
+        if (topOfScaleContainer < windowHeight - animation.revealDistance) {
+            exampleScaleContainers[i].classList.add('active');
+        } else {
+            exampleScaleContainers[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', exScale);
+window.addEventListener('load', exScale);
