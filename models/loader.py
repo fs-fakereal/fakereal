7b8c1ef785cd6a630e60140ed84a2c33f6908efa
Liam Bagabag <lbscout1@gmail.com>
2025-03-12 11:29:59 -0700

    fixing errors

--- models/loader.py
+++ models/loader.py
@@ -6,9 +6,14 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
+model_error_message: str = ""
+
 def model_generate_prediction(image_path: str, model_name: str = 'vgg16'):
+    global model_error_message
     fullname = f"deepfake-{model_name}.keras"
+
     prediction = 0
+    error_code = 0
 
     try:
         x = image.load_img(image_path)
@@ -24,12 +29,19 @@ def model_generate_prediction(image_path: str, model_name: str = 'vgg16'):
         prediction = model.predict(x_in)
         prediction = prediction[0][0]
     except Exception as e:
-        print(e)
+        model_error_message = e
+        error_code = 1
 
-    return prediction
+    return prediction, error_code
 
-def model_parse_error(error_code: int) -> str:
-    pass
+def model_get_error() -> str:
+    global model_error_message
+
+    result = model_error_message
+    model_error_message = ""
+
+    return result
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
